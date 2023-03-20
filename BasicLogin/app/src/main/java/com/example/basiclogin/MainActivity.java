17030cad69788cd25d3f1a7bf9042d901b7c145a
Panther-14 <lego.ninjago.city@gmail.com>
2023-03-20 16:57:39 -0600

    Creatin login

--- BasicLogin/app/src/main/java/com/example/basiclogin/MainActivity.java
+++ BasicLogin/app/src/main/java/com/example/basiclogin/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.basiclogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText txtPassword = findViewById(R.id.password);
+        EditText txtUsername = findViewById(R.id.username);
+        Button buttonLogin = findViewById(R.id.login);
+
+        buttonLogin.setOnClickListener(v->{
+            String password = txtPassword.getText().toString();
+            String username = txtUsername.getText().toString();
+            if(!username.isBlank() && !password.isBlank()) {
+                Toast.makeText(this,"Te has loggeado correctamente", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
